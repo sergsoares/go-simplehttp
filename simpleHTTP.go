b259a1c704a29dfab97ec040ac1d3b5bfe9d3c3e
sergiosoareszup <sergio.soares@zup.com.br>
2021-11-20 20:03:18 -0300

    Adding configuration for http messages

--- simpleHTTP.go
+++ simpleHTTP.go
@@ -31,7 +31,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
 	}
 	fmt.Fprintf(w, "\n===> local IP: %q\n\n",GetOutboundIP())
-	fmt.Fprintf(w, "Env message = %s\n", os.Getenv("message"))
+	fmt.Fprintf(w, "Env message = %s\n", "New change with HTTP for new model")
 }
 
 func GetOutboundIP() net.IP {
